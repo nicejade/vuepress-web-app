ad300c0319d5a52730beeb18752dc859960c3fec
nicejade <jeffygisgreat@gmail.com>
2018-05-30 23:13:44 +0800

    ✨ add ArticleList component

---
+++ docs/.vuepress/components/ItemList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="component-item-list">
+    <el-card
+      v-for="item in frontmatter.items" shadow="hover"
+      @click.native="handlerClick(item.link)">
+      <div class="title">{{ item.title }}</div>
+      <div class="description">
+        {{ item.description }}
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    frontmatter () {
+      return this.$page.frontmatter
+    }
+  },
+
+  mounted () {
+  },
+
+  methods: {
+    handlerClick (link) {
+      this.$router.push(this.$page.path + link + '.html')
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+.component-item-list
+  .el-card
+    margin 10px
+    cursor pointer
+    .title
+      font-size: 20px
+      padding-bottom: 4px
+      &:hover
+        color: #409eff;
+    .description
+      color: #707780
+</style>
