cad54d5f6a9eb18d476cf5c77c0e1096e53c2037
nicejade <jeffygisgreat@gmail.com>
2018-10-04 22:47:40 +0800

    ✨ rearrange the sidebars #11

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -3,12 +3,17 @@ import Element from 'element-ui'
 
 import 'element-ui/lib/theme-chalk/index.css'
 
+const helper = require('./../helper/')
+
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
   siteData // 站点元数据
 }) => {
-  // ...做一些其他的应用级别的优化
+  /* @tip: 可以在此，做一些其他的应用级别的优化 */
+
   Vue.use(Element)
+
+  helper.utils.rearrangeTheSidebars(siteData)
 }
--- docs/helper/utils.js
+++ docs/helper/utils.js
@@ -1,5 +1,49 @@
 const fs = require('fs')
 const join = require('path').join
+let gSiteData = null
+
+const _RearrangeTheSidebars = (config) => {
+  if (!config.sidebar) return
+
+  Object.keys(config.sidebar).forEach(sidebarKey => {
+    const sidebarItem = config.sidebar[sidebarKey]
+    const pageMetaArr = _FindMetaBySidebarKey(sidebarKey)
+    const newSidebarConf = _GenNewSidebarItemList(pageMetaArr)
+    if (sidebarItem && Array.isArray(sidebarItem)) {
+      sidebarItem.forEach(item => {
+        const children = item.children
+        const newChildren = []
+        newSidebarConf.forEach(item => {
+          const pureArticleName = item.path.replace(sidebarKey, '').replace('.html', '')
+          const isInChildren = children.includes(pureArticleName)
+          isInChildren ? newChildren.push(pureArticleName) : ''
+        })
+        item.children = newChildren
+      })
+    }
+  })
+}
+
+const _FindMetaBySidebarKey = (sidebarKey) => {
+  return gSiteData.pages.filter(item => {
+    return item.path.startsWith(sidebarKey)
+  })
+}
+
+const _GenNewSidebarItemList = (pageMeta = []) => {
+  const tempItemArray = pageMeta.map(item => {
+    return {
+      path: item.path,
+      datetime: item.frontmatter.datetime || item.lastUpdated
+    }
+  })
+  tempItemArray.sort((a, b) => {
+    const aDatetime = (new Date(a.datetime)).getTime()
+    const bDatetime = (new Date(b.datetime)).getTime()
+    return bDatetime - aDatetime
+  })
+  return tempItemArray
+}
 
 module.exports = {
   findSyncFolderName (startPath) {
@@ -18,9 +62,11 @@ module.exports = {
     return result
   },
 
-  /*
+  /**
     @desc: Generate Sidebar Config
-    @param: {tille, relativePath}
+    @param: {string} tille - page title
+    @param: {string} relativePath - relative path
+    @param: {boolean} isWithReadMe - is need index page
     @date: 2018-05-25
   */
   genSidebarConfig (title, relativePath = '', isWithReadMe = false) {
@@ -39,5 +85,18 @@ module.exports = {
         children: sidebarConfig
       }
     ]
+  },
+
+  /**
+    @desc: 根据文档中的 datetime 字段，为“侧边栏链接”重新排序；
+    @param: {object} siteData - 站点元数据
+    @date: 2018-10-04
+  */
+  rearrangeTheSidebars (siteData) {
+    gSiteData = siteData
+    const locales = siteData.themeConfig.locales
+    Object.values(locales).forEach(item => {
+      _RearrangeTheSidebars(item)
+    })
   }
 }
