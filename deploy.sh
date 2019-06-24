1755f344031333652980baad9953515f983eaf39
JadeYang(杨琼璞) <jeffygisgreat@gmail.com>
2019-06-24 23:46:37 +0800

    :rocket: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,5 +20,5 @@ git commit -m 'deploy update 🎉'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/nicejade/vuepress-web-app.git master:gh-pages
+git push -f git@github.com:nicejade/vuepress-web-app.git master:gh-pages
 cd -
